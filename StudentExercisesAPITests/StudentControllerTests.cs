eb2cdd295af6617ce878eb51f2d7979fc28efefd
Allison Collins <allisonkatecollins@gmail.com>
2019-03-28 14:01:57 -0500

    testing set up, not working yet

---
+++ StudentExercisesAPITests/StudentControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http;
+using Xunit;
+using StudentExercisesAPI;
+using System.Threading.Tasks;
+
+namespace StudentExercisesAPITests
+{
+    public class StudentControllerTests
+    {
+        [Fact]
+        public async Task GetSingleStudent()
+        {
+            using (HttpClient client = new APIClientProvider().Client)
+            {
+                var response = await client.GetAsync("/api/Student");
+
+                response.EnsureSuccessStatusCode();
+            }
+        }
+    }
+}
